eeb10be5f47c221e568a7a21d600be1d6af3ba93
Shekoofeh Dezhahanj <sh.dezhahanj@gmail.com>
2025-09-11 14:04:22 +0330

    feat: add not fount page

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,46 @@
+'use client';
+import { Button, Icon, Typography } from '@/components';
+import { defaultButtonClasses } from '@/components/atoms/Button/Button';
+import { useRouter } from '@/i18n/routing';
+import { useTranslations } from 'next-intl';
+import classNames from 'classnames';
+import type { FC } from 'react';
+
+export const NotFound: FC = () => {
+	const t = useTranslations('NotFound');
+	const router = useRouter();
+
+	const handleGoHome = () => {
+		router.push('/');
+	};
+
+	return (
+		<div className="m-auto min-h-screen flex w-full max-w-4xl flex-col items-center justify-center gap-6 p-8 text-center">
+			<div className="flex flex-col items-center gap-4">
+				<Typography.Title level="h1" className="text-4xl font-bold">
+					{t('title')}
+				</Typography.Title>
+				<Typography.Paragraph className="text-lg text-text-secondary">
+					{t('description')}
+				</Typography.Paragraph>
+			</div>
+
+			<div className="flex flex-col items-center gap-4">
+				<Typography.Text className="text-sm text-text-tertiary">
+					{t('errorCode')}
+				</Typography.Text>
+				<Button
+					onClick={handleGoHome}
+					className={classNames(
+						'px-6 py-3',
+						defaultButtonClasses.contained.primary,
+					)}
+				>
+					{t('goHome')}
+				</Button>
+			</div>
+		</div>
+	);
+};
+
+export default NotFound;
